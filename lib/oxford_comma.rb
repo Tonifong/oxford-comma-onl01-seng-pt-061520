6842e21537643067c0d0379284813d887c498072
Antonio O Fong <omarquinzan@yahoo.com>
2020-06-29 13:48:09 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,13 +1,13 @@
 
 def oxford_comma(array)
   
-array=["kiwi","durian"]
+  array=["kiwi","durian"]
 
   if array.size=1
-    
-  elsif array.size=2
-  return array=["kiwi","durian"].join(" and ")
+    return array="kiwi"
+    elsif array.size=2
+    return array=["kiwi","durian"].join(" and ")
+   end
  end
-end
 
   
