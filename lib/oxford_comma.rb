e36dfefa9dcf2735456833783ecedd65cc31c93f
Antonio O Fong <omarquinzan@yahoo.com>
2020-06-29 13:57:41 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,8 +3,8 @@ def oxford_comma(array)
   
   array=["kiwi","durian"]
 
-  if array.size=  1
-    return array=="kiwi"
+  if array.size== 1
+    return array="kiwi"
     elsif array.size=2
     return array=["kiwi","durian"].join(" and ")
    end
